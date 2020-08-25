df649d9d1687e9544edc2c328e045b190ff09edf
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-08-25 21:02:24 +0300

    new screen and interaction functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        backgroundColor: Colors.black
+      ),
       home: RandomWords(),
     );
   }
@@ -20,35 +24,79 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(color: Colors.white, fontSize: 18.0);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            color: Colors.white,
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+            backgroundColor: Colors.black54,
+          );
+        },
+      ),
+    );
+  }
+
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
+      /* removes weird white shadow of title */
       tileColor: Colors.black,
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : Colors.white,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
   Widget _buildSuggestions() {
-    return ListView.separated(
-      itemCount: 1000,
-      separatorBuilder: (context,index){
-        return Divider(
-          color: Colors.white,
-        );
-      },
-        padding: EdgeInsets.all(16.0),
+    return ListView.builder(
         itemBuilder:
             /*1
         The itemBuilder callback is called once per suggested word pairing, and
         places each suggestion into a ListTile row.*/
             (context, i) {
+              if (i.isOdd)
+                return Divider(
+                  color: Colors.white,
+                );
           /*2
           dd a one-pixel-high divider widget before each row in the ListView.*/
-
           final index = i ~/ 2;
           /*3
           Add a one-pixel-high divider widget before each row in the ListView.*/
@@ -68,6 +116,9 @@ class _RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         backgroundColor: Colors.black38,
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
       backgroundColor: Colors.black54,
